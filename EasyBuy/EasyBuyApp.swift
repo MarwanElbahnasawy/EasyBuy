95f686b2cfe5064d346ae93c5f84c08502f06e73
Marwan Elbahnasawy <96259021+MarwanElbahnasawy@users.noreply.github.com>
2023-05-31 23:10:31 +0300

    Initial Commit

---
+++ EasyBuy/EasyBuyApp.swift
@@ -0,0 +1,20 @@
+//
+//  EasyBuyApp.swift
+//  EasyBuy
+//
+//  Created by Marwan Elbahnasawy on 31/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct EasyBuyApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
