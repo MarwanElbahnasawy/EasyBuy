60ba2aabf1a1bd048f606b61899d85eb0439f16f
MohamedFathyAhmed <112295268+MohamedFathyAhmed@users.noreply.github.com>
2023-06-03 18:45:51 +0300

    NavigationBarView

---
+++ EasyBuy/Presentation/Home/Views/NavigationBarView.swift
@@ -0,0 +1,52 @@
+//
+//  NavigationBarView.swift
+//  EasyBuy
+//
+//  Created by mo_fathy on 01/06/2023.
+//
+
+import SwiftUI
+
+struct NavigationBarView: View {
+  // MARK: - PROPERTY
+  
+  @State var isAnimated: Bool = false
+  
+  // MARK: - BODY
+  
+  var body: some View {
+    HStack {
+      Button(action: {}, label: {
+        Image(systemName: "magnifyingglass")
+          .font(.title)
+      }) //: BUTTON
+      
+      Spacer()
+      
+      LogoView()
+        .opacity(isAnimated ? 1 : 0)
+        .offset(x: 0, y: isAnimated ? 0 : -25)
+        .onAppear(perform: {
+          withAnimation(.easeOut(duration: 0.5)) {
+            isAnimated.toggle()
+          }
+        })
+      
+      Spacer()
+      
+    
+    }.onAppear{
+        isAnimated = false
+    }//: HSTACK
+  }
+}
+
+// MARK: - PREVIEW
+
+struct NavigationBarView_Previews: PreviewProvider {
+  static var previews: some View {
+    NavigationBarView()
+      .previewLayout(.sizeThatFits)
+      .padding()
+  }
+}
