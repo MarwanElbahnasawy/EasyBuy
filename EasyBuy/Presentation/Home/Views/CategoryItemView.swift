df56663c42e5929f3a050279b63b8e447bc323b6
MohamedFathyAhmed <112295268+MohamedFathyAhmed@users.noreply.github.com>
2023-06-03 18:42:00 +0300

    CategoryItemView

---
+++ EasyBuy/Presentation/Home/Views/CategoryItemView.swift
@@ -0,0 +1,51 @@
+//
+//  CategoryItemView.swift
+//  EasyBuy
+//
+//  Created by mo_fathy on 02/06/2023.
+//
+import SwiftUI
+
+
+struct CategoryItemView: View {
+  // MARK: - PROPERTY
+  
+  let brand: Brand
+  // MARK: - BODY
+  
+  var body: some View {
+      HStack(alignment: .center, spacing: 6) {
+          KingFisherImageView(url: brand.image?.src ?? "https://theperfectroundgolf.com/wp-content/uploads/2022/04/placeholder.png",placeholder: "")
+
+          .resizable()
+          .scaledToFit()
+          .frame(width: 30, height: 30, alignment: .center)
+          .foregroundColor(.gray)
+
+          Text(brand.title?.uppercased() ?? "brand")
+          .fontWeight(.light)
+          .foregroundColor(.gray)
+
+
+        Spacer()
+      } //: HSTACK
+      .padding()
+      .background(Color.white.cornerRadius(12))
+      .background(
+        RoundedRectangle(cornerRadius: 12)
+          .stroke(Color.gray, lineWidth: 1)
+      )
+  }
+}
+
+// MARK: - PREVIEW
+
+struct CategoryItemView_Previews: PreviewProvider {
+  static var previews: some View {
+      let brand = Brand()
+    CategoryItemView(brand: brand)
+      .previewLayout(.sizeThatFits)
+      .padding()
+      .background(colorBackground)
+  }
+}
