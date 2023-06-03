50304c2e108d956d113907e18b269c301b87a7ae
MohamedFathyAhmed <112295268+MohamedFathyAhmed@users.noreply.github.com>
2023-06-03 19:21:16 +0300

    models

---
+++ EasyBuy/Presentation/Home/model/Model.swift
@@ -0,0 +1,24 @@
+//
+//  Model.swift
+//  final test
+//
+//  Created by mo_fathy on 01/06/2023.
+//
+
+import Foundation
+
+struct SmartCollections: Codable {
+    var smart_collections : [Brand]
+}
+
+struct Brand: Codable {
+    var title: String?
+    var id: Int?
+    var image : BrandImage?
+}
+
+struct BrandImage : Codable {
+    var src : String?
+}
+
+ 
---
+++ EasyBuy/Presentation/Home/model/OrderModel.swift
@@ -0,0 +1,45 @@
+import Foundation
+
+struct ResOrder: Codable {
+    var orders: [Order]
+}
+
+struct Order: Codable {
+    var id: Int?
+    var buyerAcceptsMarketing: Bool?
+    var currentTotalPrice: String?
+    var createdAt: Date?
+    var number, orderNumber: Int?
+    var token, totalDiscounts: String?
+    var totalPrice: String?
+    var shippingAddress: Address?
+    var fulfillments: [Fulfillment]?
+}
+// MARK: - Address
+
+struct Address: Codable {
+    var first_name: String?
+    var address1, phone, city, zip: String?
+    var province, country: String?
+    var last_name: String?
+    var address2: String?
+    var latitude, longitude: Double?
+    var name, country_code, province_code: String?
+    var id, customer_id: Int?
+    var country_name: String?
+}
+
+
+// MARK: - Fulfillment
+
+struct Fulfillment: Codable {
+    var id: Int?
+    var location_id: Int?
+    var name: String?
+    var order_id: Int?
+    var service: String?
+    var status, tracking_company, tracking_number: String?
+    var tracking_numbers: [String]?
+    var tracking_url: String?
+    var tracking_urls: [String]?
+}
