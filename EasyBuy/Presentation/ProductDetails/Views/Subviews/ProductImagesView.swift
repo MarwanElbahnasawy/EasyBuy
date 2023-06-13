9aa79fd6763caad23d29cecae879f078a27934e9
AhmadHemeda <hemeda17@gmail.com>
2023-06-14 01:22:06 +0300

    fixed product details to display all product images not just the featured image

---
+++ EasyBuy/Presentation/ProductDetails/Views/Subviews/ProductImagesView.swift
@@ -0,0 +1,30 @@
+import SwiftUI
+import Kingfisher
+
+struct ProductImagesView: View {
+    var images: [FeaturedImage]?
+    
+    var body: some View {
+        if let images = images, !images.isEmpty {
+            GeometryReader { geometry in
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(images, id: \.id) { image in
+                            KFImage(URL(string: image.url ?? ""))
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: geometry.size.width, height: geometry.size.width, alignment: .center)
+                        }
+                    }
+                }
+            }
+            .frame(height: UIScreen.main.bounds.width)
+        }
+    }
+}
+
+struct ProductImagesView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductImagesView()
+    }
+}
