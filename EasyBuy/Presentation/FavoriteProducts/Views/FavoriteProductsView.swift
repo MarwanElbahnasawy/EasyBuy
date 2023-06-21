d3f7a5970b89bfd5df2fba3d25e1c0f7328cb77d
AhmadHemeda <hemeda17@gmail.com>
2023-06-21 04:33:30 +0300

    fixed some bugs in both profile view and favorite view

--- EasyBuy/Presentation/FavoriteProducts/Views/FavoriteProductsView.swift
+++ EasyBuy/Presentation/FavoriteProducts/Views/FavoriteProductsView.swift
@@ -9,41 +9,44 @@ struct FavoriteProductsView: View {
 
     var body: some View {
         VStack {
-            SearchBar(text: $searchText, isEditing: $isSearching)
-
             Text("Wishlist")
                 .font(.title)
                 .fontWeight(.bold)
                 .padding()
 
             ScrollView {
-                LazyVStack {
-                    ForEach(favoriteProductViewModel.products ?? [], id: \.variant?.id) { item in
-                        FavoriteAndCartCell(
-                            imageUrl: URL(string: item.product?.featuredImage?.url ?? "not available"),
-                            title: item.product?.title,
-                            type: item.product?.productType,
-                            price: item.product?.priceRangeV2?.maxVariantPrice?.amount,
-                            onDelete: {
-                                showAlert = true
-                                indexSet = favoriteProductViewModel.indexSet(for: item) ?? IndexSet()
-                            }
-                        )
-                        .alert(isPresented: $showAlert) {
-                            Alert(
-                                title: Text("Delete?"),
-                                message: Text("Do you want to delete this product from your favorite?"),
-                                primaryButton: .destructive(Text("OK"), action: {
-                                    if let indexSet = indexSet {
-                                        favoriteProductViewModel.deleteFavoriteProduct(indexSet: indexSet)
-                                    }
-                                }),
-                                secondaryButton: .cancel()
+                if let products = favoriteProductViewModel.products, !products.isEmpty {
+                    LazyVStack {
+                        ForEach(products, id: \.variant?.id) { item in
+                            FavoriteAndCartCell(
+                                imageUrl: URL(string: item.product?.featuredImage?.url ?? "not available"),
+                                title: item.product?.title,
+                                type: item.product?.productType,
+                                price: formatPrice(price: item.product?.priceRangeV2?.maxVariantPrice?.amount),
+                                onDelete: {
+                                    showAlert = true
+                                    indexSet = favoriteProductViewModel.indexSet(for: item) ?? IndexSet()
+                                },
+                                isFavorite: true
                             )
+                            .alert(isPresented: $showAlert) {
+                                Alert(
+                                    title: Text("Delete?"),
+                                    message: Text("Do you want to delete this product from your favorite?"),
+                                    primaryButton: .destructive(Text("OK"), action: {
+                                        if let indexSet = indexSet {
+                                            favoriteProductViewModel.deleteFavoriteProduct(indexSet: indexSet)
+                                        }
+                                    }),
+                                    secondaryButton: .cancel()
+                                )
+                            }
                         }
                     }
+                    .padding(.horizontal)
+                } else {
+                    NoFavorites()
                 }
-                .padding(.horizontal)
             }
         }
         .onAppear {
